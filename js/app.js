1e176cf04885bdd54f3c516ac73daf6910ff3844
DavidLZisele <davidzisele2016@gmail.com>
2021-02-15 19:02:13 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+
+window.onload = function()
+{
+   var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+    Array.from(document.querySelectorAll('.div-footer')).forEach(div => {
+         div.firstElementChild.onmouseover = function()
+         {
+            div.lastElementChild.classList.remove('d-none')
+            div.lastElementChild.classList.add('divFooter')
+         }
+         div.firstElementChild.onmouseout = function()
+         {
+            div.lastElementChild.classList.add('d-none')
+            div.lastElementChild.classList.remove('divFooter')
+         }
+    })
+    Array.from(document.querySelectorAll('.nav-item')).forEach(div => {
+        div.firstElementChild.onmouseover = function()
+        {
+           div.lastElementChild.classList.remove('d-none')
+           div.lastElementChild.classList.add('divHeader')
+        }
+        div.firstElementChild.onmouseout = function()
+        {
+           div.lastElementChild.classList.add('d-none')
+           div.lastElementChild.classList.remove('divHeader')
+        }
+   })
+   ScrollReveal().reveal('#destacados',{delay: 500})
+   ScrollReveal().reveal('#novedades', {delay: 500})
+}
