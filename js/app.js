f77b185baf79d909349c338297b1c2b346c6e9e1
DavidLZisele <davidzisele2016@gmail.com>
2021-02-18 05:34:16 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ window.onload = function()
         }
    })
    ScrollReveal().reveal('#novedades', {delay: 500})
-   if(screen.width > 992)
+   if(screen.width >= 1200)
    {
       ScrollReveal().reveal('.producto1', {delay: 600})
       ScrollReveal().reveal('.producto2', {delay: 700})
@@ -50,8 +50,19 @@ window.onload = function()
       ScrollReveal().reveal('.producto4', {delay: 900})
       ScrollReveal().reveal('.producto5', {delay: 1000})
       ScrollReveal().reveal('.producto6', {delay: 1100})
-      ScrollReveal().reveal('.producto7', {delay: 500})
-      ScrollReveal().reveal('.producto8', {delay: 600})
+      ScrollReveal().reveal('.producto7', {delay: 1200})
+      ScrollReveal().reveal('.producto8', {delay: 1300})
+   } else if(screen.width >= 992 && screen.width <1200)
+   {
+      ScrollReveal().reveal('.producto1', {delay: 600})
+      ScrollReveal().reveal('.producto2', {delay: 700})
+      ScrollReveal().reveal('.producto3', {delay: 800})
+      ScrollReveal().reveal('.producto4', {delay: 900})
+      ScrollReveal().reveal('.producto5', {delay: 1000})
+      ScrollReveal().reveal('.producto6', {delay: 1100})
+      ScrollReveal().reveal('.producto7', {delay: 100})
+      ScrollReveal().reveal('.producto8', {delay: 200})
+   
    } else 
    {
       ScrollReveal().reveal('.producto1', {delay: 500})
@@ -62,7 +73,6 @@ window.onload = function()
       ScrollReveal().reveal('.producto6', {delay: 500})
       ScrollReveal().reveal('.producto7', {delay: 500})
       ScrollReveal().reveal('.producto8', {delay: 500})
-   
    }
   
 }
