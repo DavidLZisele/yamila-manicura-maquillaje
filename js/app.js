e375bd8ee8920c774ee359e6e641a487eb24e3bc
DavidLZisele <davidzisele2016@gmail.com>
2021-04-03 03:19:33 -0300

    listo

--- js/app.js
+++ js/app.js
@@ -43,62 +43,54 @@ window.onload = function()
    })
    ScrollReveal().reveal('#novedades', {delay: 500})
    if(screen.width >= 1200)
-   {      
-      ScrollReveal().reveal('.producto1', {delay: 500})
-      ScrollReveal().reveal('.producto2', {delay: 600})
-      ScrollReveal().reveal('.producto3', {delay: 700})
-      ScrollReveal().reveal('.producto4', {delay: 800})
-      ScrollReveal().reveal('.producto5', {delay: 500})
-      ScrollReveal().reveal('.producto6', {delay: 600})
-      ScrollReveal().reveal('.producto7', {delay: 700})
-      ScrollReveal().reveal('.producto8', {delay: 800})
-      ScrollReveal().reveal('.producto9', {delay: 500})
-      ScrollReveal().reveal('.producto10', {delay: 600})
-      ScrollReveal().reveal('.producto11', {delay: 700})
-      ScrollReveal().reveal('.producto12', {delay: 800})
-      ScrollReveal().reveal('.producto13', {delay: 500})
-      ScrollReveal().reveal('.producto14', {delay: 600})
-      ScrollReveal().reveal('.producto15', {delay: 700})
-      ScrollReveal().reveal('.producto16', {delay: 800})
-      ScrollReveal().reveal('.producto17', {delay: 500})
+   {     
+      let i=0; 
+      delay_=500;
+      cant =  Array.from(document.querySelector('#productos').children).length;
+     for(let j=1 ; j<=cant;j++)
+      {
+         ScrollReveal().reveal('.producto' + j, {delay: delay_})
+         if(i<3)
+         {
+            i++;
+            delay_ += 100;
+         } else 
+         {        
+            i=0;
+            delay_=500;   
+         }
+         
+      }
+       
    } else if(screen.width >= 768 && screen.width <1200)
    {
-      ScrollReveal().reveal('.producto1', {delay: 500})
-      ScrollReveal().reveal('.producto2', {delay: 600})
-      ScrollReveal().reveal('.producto3', {delay: 700})
-      ScrollReveal().reveal('.producto4', {delay: 500})
-      ScrollReveal().reveal('.producto5', {delay: 600})
-      ScrollReveal().reveal('.producto6', {delay: 700})
-      ScrollReveal().reveal('.producto7', {delay: 500})
-      ScrollReveal().reveal('.producto8', {delay: 600})
-      ScrollReveal().reveal('.producto9', {delay: 700})
-      ScrollReveal().reveal('.producto10', {delay: 500})
-      ScrollReveal().reveal('.producto11', {delay: 600})
-      ScrollReveal().reveal('.producto12', {delay: 700})
-      ScrollReveal().reveal('.producto13', {delay: 500})
-      ScrollReveal().reveal('.producto14', {delay: 600})
-      ScrollReveal().reveal('.producto15', {delay: 700})
-      ScrollReveal().reveal('.producto16', {delay: 500})
-      ScrollReveal().reveal('.producto17', {delay: 600})
+      let i=0; 
+      delay_=500;
+      cant =  Array.from(document.querySelector('#productos').children).length;
+     for(let j=1 ; j<=cant;j++)
+      {
+         ScrollReveal().reveal('.producto' + j, {delay: delay_})
+         console.log(j)
+         if(i<2)
+         {
+            i++;
+            delay_ += 100;
+         } else 
+         {        
+            i=0;
+            delay_=500;   
+         }        
+      }
    } else 
    {
+      let i=0; 
+      delay_=500;
+      cant =  Array.from(document.querySelector('#productos').children).length;
       ScrollReveal().reveal('.producto1', {delay: 1200})
-      ScrollReveal().reveal('.producto2', {delay: 500})
-      ScrollReveal().reveal('.producto3', {delay: 500})
-      ScrollReveal().reveal('.producto4', {delay: 500})
-      ScrollReveal().reveal('.producto5', {delay: 500})
-      ScrollReveal().reveal('.producto6', {delay: 500})
-      ScrollReveal().reveal('.producto7', {delay: 500})
-      ScrollReveal().reveal('.producto8', {delay: 500})
-      ScrollReveal().reveal('.producto9', {delay: 500})
-      ScrollReveal().reveal('.producto10', {delay: 500})
-      ScrollReveal().reveal('.producto11', {delay: 500})
-      ScrollReveal().reveal('.producto12', {delay: 500})
-      ScrollReveal().reveal('.producto13', {delay: 500})
-      ScrollReveal().reveal('.producto14', {delay: 500})
-      ScrollReveal().reveal('.producto15', {delay: 500})
-      ScrollReveal().reveal('.producto16', {delay: 500})
-      ScrollReveal().reveal('.producto17', {delay: 500})
+      for(let j=2 ; j<=cant;j++)
+      {
+         ScrollReveal().reveal('.producto' + j, {delay: delay_})     
+      }
    }   
             document.querySelector('.li-mat').onmouseover = function()
             {
